009a8c1caacaf3d411c8c111d547dd5b5fa380bf
Yashwanth S <yashwanth.siva@aspiresys.com>
2021-09-06 09:26:43 +0530

    Add project files.

---
+++ WebAPI/Content/Scripts/Product.js
@@ -0,0 +1,102 @@
+﻿$(document).ready(function () {
+    getProductData();
+})
+
+function SaveProduct() {
+    var url = "/api/Product";
+    var objectProduct = {};
+    objectProduct.Name = $('#txtProductName').val();
+    objectProduct.Price = $('#txtProductPrice').val();
+    objectProduct.Quantity = $('#txtProductQuantity').val();
+    objectProduct.Active = 1;
+    if (objectProduct) {
+        $.ajax({
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(objectProduct),
+            type: "Post",
+            success: function (result) {
+                clear();
+                //alert(result);
+                getProductData();
+            },
+            error: function (msg) {
+                alert(msg);
+            }
+        });
+    }
+}  
+function clear() {
+    $('#txtProductName').val('');
+    $('#txtProductPrice').val('');
+    $('#txtProductQuantity').val('');
+}
+function getProductData() {
+    var url = "/api/Product";
+    $.ajax({
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        type: "Get",
+        success: function (result) {
+            //alert(result);
+            if (result) {
+                $('#tblProductBody').html('');
+                var row = '';
+                for (let i = 0; i < result.length; i++) {
+                    row = row
+                        + "<tr>"
+                        + "<td>" + result[i].Name + "</td>"
+                        + "<td>" + result[i].Price + "</td>"
+                        + "<td>" + result[i].Quantity + "</td>"
+                        + "<td>" + result[i].Active + "</td>"
+                        + "<td> <button class = 'btn btn-primary' onclick='DeleteProduct(" + result[i].Id + ")'>Delete</button></td>"
+                        /*+"<td> <button class = 'btn btn-primary' >Edit</button></td>"*/
+                        + "</tr>";
+                }
+                if (row != '') {
+                    $('#tblProductBody').append(row);
+                }
+            }
+        },
+        error: function (msg) {
+            alert(msg);
+        }
+    });
+}
+function DeleteProduct(id) {
+    var url = "/api/Product/"+id;
+        $.ajax({
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            type: "Delete",
+            success: function (result) {
+                clear();
+                alert(result);
+                getProductData();
+            },
+            error: function (msg) {
+                alert(msg);
+            }
+        });
+    
+}  
+//function EditProduct {
+//    var url = "/api/Product";
+//    $.ajax({
+//        url: url,
+//        contentType: "application/json; charset=utf-8",
+//        dataType: "json",
+//        type: "Put",
+//        success: function (result) {
+//            clear();
+//            alert(result);
+//            getProductData();
+//        },
+//        error: function (msg) {
+//            alert(msg);
+//        }
+//    });
+//}
---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        ProductEntities db = new ProductEntities();
+
+        //post
+        public string Post(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return "Product added";
+        }
+
+        //Get
+        public IEnumerable<Product> Get()
+        {
+            return db.Products.ToList();
+        }
+
+        //Get single product
+        public Product Get(int id)
+        {
+           Product product= db.Products.Find(id);
+            return product;
+        }
+
+        //update
+        public string Put(int id, Product product)
+        {
+            var product_ = db.Products.Find(id);
+            product_.Name = product.Name;
+            product_.Price = product.Price;
+            product_.Quantity = product.Quantity;
+            product_.Active = product.Active;
+
+            db.Entry(product_).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return "Product updated";
+                 
+        }
+
+        public string Delete(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return "Deleted";
+        }
+    }
+}
